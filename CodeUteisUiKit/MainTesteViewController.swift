6ede4329ea927e7451beb4f7d85cce2e532ac468
edy alves <30.edyalves@gmail.com>
2024-12-15 17:35:58 -0300

    Incluindo alterações

--- CodeUteisUiKit/MainTesteViewController.swift
+++ CodeUteisUiKit/MainTesteViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class MainTesteViewController: UIViewController {
 
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var textName: UITextField!
+    
+    @IBOutlet weak var indicatorLoading: UIActivityIndicatorView!
+    @IBOutlet weak var textLoading: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        indicatorLoading.isHidden = true
+        textLoading.isHidden = true
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func Enviar(_ sender: UIButton) {
+        labelName.text = textName.text
+        indicatorLoading.isHidden = false
+        indicatorLoading.startAnimating()
+        textLoading.isHidden = false
+        
+    }
+    
     /*
     // MARK: - Navigation
 
